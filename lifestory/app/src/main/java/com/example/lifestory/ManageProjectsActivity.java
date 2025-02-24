69995af41f1e86d231148636aaa6e867a670d288
FbeN31 <frolytheking@gmail.com>
2025-02-24 12:27:23 +0200

    added changes

--- lifestory/app/src/main/java/com/example/lifestory/ManageProjectsActivity.java
+++ lifestory/app/src/main/java/com/example/lifestory/ManageProjectsActivity.java
@@ -1,24 +1,24 @@
 package com.example.lifestory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ManageProjectsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manage_projects);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button backToHomeButton = findViewById(R.id.backToHomeBtn);
+        backToHomeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ManageProjectsActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
